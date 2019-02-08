6723597e7096504d1ab2828b829b7cd4f675ffa2
tabdelmaguid <tarek.abdelmaguid@uptake.com>
2019-02-07 20:41:51 -0600

    Set a cell to a symbol if it's missing from all other cells in a group

--- src/main/scala/org/tabdelmaguid/sudoku_actor/Cell.scala
+++ src/main/scala/org/tabdelmaguid/sudoku_actor/Cell.scala
@@ -25,9 +25,26 @@ class Cell(id: Int) extends Actor with ActorLogging {
   val groupOptions: mutable.Map[String, List[CellOptions]] = mutable.Map()
   var myOptions: Set[Byte] = ALL_SYMBOLS
 
+  /** iterate over a list, applying a function. If the result satisfies a condition, return the result
+      if not, keep going. If non match, return default */
+  def iterateUntil[E, T](list: List[E], cond: T => Boolean, fun: E => T): Option[T] = list match {
+      case Nil => None
+      case head :: tail =>
+        val calculation = fun(head)
+        if (cond(calculation)) Some(calculation) else iterateUntil(tail, cond, fun)
+  }
+
+
   def calcNewOptions(): Set[Byte] = {
     val usedSymbols = groupOptions.values.flatten.map(_.options).filter(_.size == 1).reduce(_ ++ _)
-    myOptions -- usedSymbols
+    val unusedSymbols = myOptions -- usedSymbols
+
+    val noShows = iterateUntil[List[CellOptions], Set[Byte]](groupOptions.values.toList, _.size == 1, { group =>
+      group.foldLeft(unusedSymbols) { (acc, cell) => acc -- cell.options }
+    })
+
+    if (noShows.isEmpty || noShows.get.size > 1) unusedSymbols
+    else noShows.get
   }
 
   private def myNeighbors= neighborsOptions.keys
@@ -37,13 +54,12 @@ class Cell(id: Int) extends Actor with ActorLogging {
     myNeighbors.foreach(_ ! MyOptions(myOptions))
   }
 
-  private def updateState(newOptions: Set[Byte]): Unit = {
+  private def updateState(newOptions: Set[Byte]): Unit =
     if (newOptions != myOptions) {
       myOptions = newOptions
       broadcastState()
       if (newOptions.size == 1) context.stop(self)
     }
-  }
 
   override def receive: Receive = {
     case AddNeighbors(groupKey, neighbors) =>
